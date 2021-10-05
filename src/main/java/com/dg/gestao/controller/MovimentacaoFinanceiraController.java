ee5b2dd51def3f94fbdf2b3e8b00d58afc330a39
David <davi.alfredo@hotmail.com>
2021-10-05 09:01:37 -0300

    Adicionando remoção e atualização Movimentações

--- src/main/java/com/dg/gestao/controller/MovimentacaoFinanceiraController.java
+++ src/main/java/com/dg/gestao/controller/MovimentacaoFinanceiraController.java
@@ -7,10 +7,13 @@ import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.orm.jpa.JpaObjectRetrievalFailureException;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -75,5 +78,31 @@ public class MovimentacaoFinanceiraController {
 			retorno = repository.getByTipo(tipo);
 		return new ResponseEntity<>(retorno, HttpStatus.OK);
 	}
+	
+	@Operation(description = "Remover uma movimentação")
+	@DeleteMapping(value="/movimentacoes/{id}")
+	public ResponseEntity<?> removeCliente(@PathVariable final Long id) {	
+		try {
+			MovimentacaoFinanceiraModel model = repository.getById(id);
+			repository.delete(model);
+			return new ResponseEntity<>("Removido com sucesso!", HttpStatus.OK);
+		}catch(JpaObjectRetrievalFailureException e) {
+			return new ResponseEntity<>("Nenhum registro encontrado para o ID informado.", HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@Operation(description = "Atualizar uma movimentação")
+	@PutMapping(value="/movimentacoes")
+	public ResponseEntity<?> updateCliente(@RequestBody final MovimentacaoFinanceiraModel movimentacao) {
+		try {		
+			if(repository.existsById(movimentacao.getId()))
+				return new ResponseEntity<>(repository.save(movimentacao), HttpStatus.OK);
+			else 
+				return new ResponseEntity<>("Nenhum registro encontrado para o ID informado.", HttpStatus.NOT_FOUND);
+		}catch(JpaObjectRetrievalFailureException e) {
+			return new ResponseEntity<>("Nenhum registro encontrado para o ID informado.", HttpStatus.NOT_FOUND);
+		}
+	}
+
 
 }
