9825831100033de650de14d41dcf57ec49d97553
David <davi.alfredo@hotmail.com>
2021-10-15 19:16:08 -0300

    Cors

--- src/main/java/com/dg/gestao/config/CorsConfiguration.java
+++ src/main/java/com/dg/gestao/config/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
 	 @Override
 	    public void addCorsMappings(CorsRegistry registry) {
 	        registry.addMapping("/**")
-	            .allowedOrigins("*")
+	            .allowedOrigins("https://platform.preview.appgyver.com")
 	            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
 	    }
 }
